39d4e05f4c7daf1e083a2b7ef29cf9838ca26570
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2024-12-07 22:01:46 +0900

    Part 2

--- src/solutions/day07.rs
+++ src/solutions/day07.rs
@@ -2,20 +2,6 @@ use super::Solution;
 
 pub struct Day07;
 
-enum Part1 {
-    Add,
-    Mul,
-}
-
-impl Part1 {
-    fn apply(&self, a: usize, b: usize) -> usize {
-        match self {
-            Part1::Add => a + b,
-            Part1::Mul => a * b,
-        }
-    }
-}
-
 impl Solution for Day07 {
     fn test_input() -> String {
         String::from(
@@ -35,10 +21,8 @@ impl Solution for Day07 {
         fn backtrack(target: usize, now: usize, nums: &[usize]) -> bool {
             if now > target {
                 false
-            } else if now == target {
-                true
             } else if nums.len() == 0 {
-                false
+                now == target
             } else {
                 backtrack(target, now + nums[0], &nums[1..])
                     || backtrack(target, now * nums[0], &nums[1..])
@@ -65,7 +49,44 @@ impl Solution for Day07 {
     }
 
     fn solve_part_2(_input: String) -> String {
-        String::from("0")
+        fn concat(mut a: usize, b: usize) -> usize {
+            let mut n = b;
+            while n > 0 {
+                a *= 10;
+                n /= 10;
+            }
+            a + b
+        }
+
+        fn backtrack(target: usize, now: usize, nums: &[usize]) -> bool {
+            if now > target {
+                false
+            } else if nums.len() == 0 {
+                now == target
+            } else {
+                backtrack(target, now + nums[0], &nums[1..])
+                    || backtrack(target, now * nums[0], &nums[1..])
+                    || backtrack(target, concat(now, nums[0]), &nums[1..])
+            }
+        }
+        let mut total = 0;
+
+        for line in _input.lines() {
+            let mut words = line.split(":");
+            let ans: usize = words.next().unwrap().parse().unwrap();
+            let nums: Vec<usize> = words
+                .next()
+                .unwrap()
+                .trim()
+                .split_ascii_whitespace()
+                .map(|w| w.parse().unwrap())
+                .collect();
+            if backtrack(ans, nums[0], &nums[1..]) {
+                total += ans;
+            }
+        }
+
+        total.to_string()
     }
 }
 
@@ -84,6 +105,6 @@ mod day07_tests {
     fn test_part_2() {
         let input = Day07::test_input();
         let ans = Day07::solve_part_2(input);
-        assert_eq!(ans, "");
+        assert_eq!(ans, "11387");
     }
 }
