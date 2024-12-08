dbda1c2536f03d3e800306b93c349ab28354923d
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2024-12-08 14:17:07 +0900

    part 1'

--- src/solutions/day08.rs
+++ src/solutions/day08.rs
@@ -1,14 +1,87 @@
+use std::collections::{HashMap, HashSet};
+
 use super::Solution;
 
 pub struct Day08;
 
+fn num_antinodes(
+    bound: (usize, usize),
+    antennas: &[(usize, usize)],
+    seen: &mut HashSet<(i32, i32)>,
+) {
+    let (mx, my) = bound;
+    let mx = mx as i32;
+    let my = my as i32;
+    let l = antennas.len();
+    for i in 0..l {
+        for j in (i + 1)..l {
+            let a = antennas[i];
+            let b = antennas[j];
+            let (x1, y1) = a;
+            let (x2, y2) = b;
+            let x1 = x1 as i32;
+            let x2 = x2 as i32;
+            let y1 = y1 as i32;
+            let y2 = y2 as i32;
+            let dx = x2 - x1;
+            let dy = y2 - y1;
+            let x = x1 - dx;
+            let y = y1 - dy;
+            if 0 <= x && x < mx && 0 <= y && y < my {
+                seen.insert((x, y));
+            }
+            let x = x2 + dx;
+            let y = y2 + dy;
+            if 0 <= x && x < mx && 0 <= y && y < my {
+                seen.insert((x, y));
+            }
+        }
+    }
+}
+
 impl Solution for Day08 {
     fn test_input() -> String {
-        String::from("")
+        String::from(
+            "............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............",
+        )
     }
 
     fn solve_part_1(_input: String) -> String {
-        String::from("0")
+        let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+
+        let map: Vec<Vec<char>> = _input.lines().map(|line| line.chars().collect()).collect();
+        let bound = (map.len(), map[0].len());
+
+        for (r, row) in map.iter().enumerate() {
+            for (c, ch) in row.iter().enumerate() {
+                match ch {
+                    '.' => {}
+                    ch => {
+                        let v = antennas.entry(*ch).or_insert(vec![]);
+                        v.push((r, c));
+                    }
+                }
+            }
+        }
+
+        let mut seen: HashSet<(i32, i32)> = HashSet::new();
+
+        for (_key, poses) in antennas.iter() {
+            num_antinodes(bound, poses, &mut seen);
+        }
+
+        seen.len().to_string()
     }
 
     fn solve_part_2(_input: String) -> String {
@@ -24,7 +97,7 @@ mod day08_tests {
     fn test_part_1() {
         let input = Day08::test_input();
         let ans = Day08::solve_part_1(input);
-        assert_eq!(ans, "");
+        assert_eq!(ans, "14");
     }
 
     #[test]
