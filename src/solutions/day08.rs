ad125ef5f2fe85f40150bf8a2b950517ae8a9eca
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2024-12-08 14:24:28 +0900

    Part 2

--- src/solutions/day08.rs
+++ src/solutions/day08.rs
@@ -4,6 +4,19 @@ use super::Solution;
 
 pub struct Day08;
 
+fn gcd(a: i32, b: i32) -> i32 {
+    if b < a {
+        gcd(b, a)
+    } else {
+        let rem = b % a;
+        if rem == 0 {
+            a
+        } else {
+            gcd(rem, a)
+        }
+    }
+}
+
 fn num_antinodes(
     bound: (usize, usize),
     antennas: &[(usize, usize)],
@@ -34,6 +47,60 @@ fn num_antinodes(
             let y = y2 + dy;
             if 0 <= x && x < mx && 0 <= y && y < my {
                 seen.insert((x, y));
+            }
+        }
+    }
+}
+
+fn num_antinodes_2(
+    bound: (usize, usize),
+    antennas: &[(usize, usize)],
+    seen: &mut HashSet<(i32, i32)>,
+) {
+    let (mx, my) = bound;
+    let mx = mx as i32;
+    let my = my as i32;
+    let l = antennas.len();
+    for i in 0..l {
+        for j in (i + 1)..l {
+            let a = antennas[i];
+            let b = antennas[j];
+            let (x1, y1) = a;
+            let (x2, y2) = b;
+            let x1 = x1 as i32;
+            let x2 = x2 as i32;
+            let y1 = y1 as i32;
+            let y2 = y2 as i32;
+            let dx = x2 - x1;
+            let dy = y2 - y1;
+            let d = gcd(dx.abs(), dy.abs());
+            let dx = dx / d;
+            let dy = dy / d;
+            let mut x = x1;
+            let mut y = y1;
+            seen.insert((x1, y1));
+            seen.insert((x2, y2));
+            loop {
+                x -= dx;
+                y -= dy;
+                if x < 0 || x >= mx || y < 0 || y >= my {
+                    break;
+                } else {
+                    seen.insert((x, y));
+                }
+            }
+            let mut x = x1;
+            let mut y = y1;
+            loop {
+                x += dx;
+                y += dy;
+                if x == x2 && y == y2 {
+                    continue;
+                } else if x < 0 || x >= mx || y < 0 || y >= my {
+                    break;
+                } else {
+                    seen.insert((x, y));
+                }
             }
         }
     }
@@ -85,7 +152,30 @@ impl Solution for Day08 {
     }
 
     fn solve_part_2(_input: String) -> String {
-        String::from("0")
+        let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
+
+        let map: Vec<Vec<char>> = _input.lines().map(|line| line.chars().collect()).collect();
+        let bound = (map.len(), map[0].len());
+
+        for (r, row) in map.iter().enumerate() {
+            for (c, ch) in row.iter().enumerate() {
+                match ch {
+                    '.' => {}
+                    ch => {
+                        let v = antennas.entry(*ch).or_insert(vec![]);
+                        v.push((r, c));
+                    }
+                }
+            }
+        }
+
+        let mut seen: HashSet<(i32, i32)> = HashSet::new();
+
+        for (_key, poses) in antennas.iter() {
+            num_antinodes_2(bound, poses, &mut seen);
+        }
+
+        seen.len().to_string()
     }
 }
 
@@ -104,6 +194,6 @@ mod day08_tests {
     fn test_part_2() {
         let input = Day08::test_input();
         let ans = Day08::solve_part_2(input);
-        assert_eq!(ans, "");
+        assert_eq!(ans, "34");
     }
 }
