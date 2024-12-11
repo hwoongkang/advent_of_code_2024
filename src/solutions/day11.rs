aa33854948a9ad8402afc66209407dcc7ea6025e
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2024-12-11 17:48:58 +0900

    Part 2

--- src/solutions/day11.rs
+++ src/solutions/day11.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use super::Solution;
 
 fn num_digits(mut n: usize) -> usize {
@@ -32,10 +34,15 @@ impl PlutoStone {
     }
 }
 
-fn blink(stones: Vec<PlutoStone>) -> Vec<PlutoStone> {
-    let mut ans = vec![];
-    for stone in stones.iter() {
-        ans.append(&mut stone.blink());
+fn blink_map(stones: HashMap<usize, usize>) -> HashMap<usize, usize> {
+    let mut ans = HashMap::new();
+    for (num, qty) in stones.into_iter() {
+        let stone = PlutoStone(num);
+        for stone in stone.blink() {
+            let num = stone.0;
+            let entry = ans.entry(num).or_insert(0);
+            *entry += qty;
+        }
     }
     ans
 }
@@ -48,27 +55,29 @@ impl Solution for Day11 {
     }
 
     fn solve_part_1(_input: String) -> String {
-        let mut stones = _input
-            .split_ascii_whitespace()
-            .map(|w| w.parse().unwrap())
-            .map(|n| PlutoStone(n))
-            .collect();
-        for _ in 0..25 {
-            stones = blink(stones);
+        let mut stones = HashMap::new();
+        for n in _input.split_ascii_whitespace().map(|w| w.parse().unwrap()) {
+            let entry = stones.entry(n).or_insert(0);
+            *entry += 1;
         }
-        stones.len().to_string()
+
+        for _ in 0..25 {
+            stones = blink_map(stones);
+        }
+        stones.into_values().sum::<usize>().to_string()
     }
 
     fn solve_part_2(_input: String) -> String {
-        let mut stones = _input
-            .split_ascii_whitespace()
-            .map(|w| w.parse().unwrap())
-            .map(|n| PlutoStone(n))
-            .collect();
-        for _ in 0..40 {
-            stones = blink(stones);
+        let mut stones = HashMap::new();
+        for n in _input.split_ascii_whitespace().map(|w| w.parse().unwrap()) {
+            let entry = stones.entry(n).or_insert(0);
+            *entry += 1;
         }
-        stones.len().to_string()
+
+        for _ in 0..75 {
+            stones = blink_map(stones);
+        }
+        stones.into_values().sum::<usize>().to_string()
     }
 }
 
@@ -77,24 +86,10 @@ mod day11_tests {
     use super::*;
 
     #[test]
-    fn test_blink() {
-        let prev = vec![
-            PlutoStone(0),
-            PlutoStone(1),
-            PlutoStone(10),
-            PlutoStone(99),
-            PlutoStone(999),
-        ];
-        let next = vec![
-            PlutoStone(1),
-            PlutoStone(2024),
-            PlutoStone(1),
-            PlutoStone(0),
-            PlutoStone(9),
-            PlutoStone(9),
-            PlutoStone(2021976),
-        ];
-        assert_eq!(blink(prev), next);
+    fn test_blink_map() {
+        let prev = HashMap::from([(0, 1), (1, 1), (10, 1), (99, 1), (999, 1)]);
+        let next = HashMap::from([(0, 1), (1, 2), (9, 2), (2024, 1), (2021976, 1)]);
+        assert_eq!(blink_map(prev), next);
     }
 
     #[test]
@@ -102,12 +97,5 @@ mod day11_tests {
         let input = Day11::test_input();
         let ans = Day11::solve_part_1(input);
         assert_eq!(ans, "55312");
-    }
-
-    #[test]
-    fn test_part_2() {
-        let input = Day11::test_input();
-        let ans = Day11::solve_part_2(input);
-        assert_eq!(ans, "");
     }
 }
