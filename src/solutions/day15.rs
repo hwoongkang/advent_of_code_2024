37cb9c6d35b38fe61aa4ae3a2e303f58cce6d1cd
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2024-12-17 12:26:31 +0900

    Part 2

--- src/solutions/day15.rs
+++ src/solutions/day15.rs
@@ -10,6 +10,14 @@ enum Tile {
 }
 
 #[derive(PartialEq, Eq, Debug, Copy, Clone)]
+enum Tile2 {
+    Wall,
+    BoxLeft,
+    BoxRight,
+    Empty,
+}
+
+#[derive(PartialEq, Eq, Debug, Copy, Clone)]
 enum Dir {
     Up,
     Down,
@@ -130,7 +138,7 @@ impl System {
         }
         ans
     }
-    fn print(&self) {
+    fn _print(&self) {
         for row in self.map.iter() {
             for tile in row.iter() {
                 match tile {
@@ -141,6 +149,168 @@ impl System {
             }
             println!();
         }
+    }
+}
+#[derive(PartialEq, Eq, Debug)]
+struct System2 {
+    robot: (usize, usize),
+    map: Vec<Vec<Tile2>>,
+}
+
+impl System2 {
+    fn from(input: &str) -> Self {
+        let mut robot: (usize, usize) = (0, 0);
+        let map: Vec<Vec<Tile2>> = input
+            .lines()
+            .enumerate()
+            .map(|(r, line)| {
+                line.chars()
+                    .enumerate()
+                    .map(|(c, char)| match char {
+                        '#' => [Tile2::Wall, Tile2::Wall],
+                        '.' => [Tile2::Empty, Tile2::Empty],
+                        'O' => [Tile2::BoxLeft, Tile2::BoxRight],
+                        '@' => {
+                            robot = (r, 2 * c);
+                            [Tile2::Empty, Tile2::Empty]
+                        }
+                        _ => unimplemented!(),
+                    })
+                    .flatten()
+                    .collect()
+            })
+            .collect();
+        Self { robot, map }
+    }
+
+    fn size(&self) -> (usize, usize) {
+        (self.map.len(), self.map[0].len())
+    }
+
+    fn _print(&self) {
+        let (mr, mc) = self.size();
+        let mut new_map: Vec<Vec<char>> = vec![vec!['.'; mc]; mr];
+        for (r, row) in self.map.iter().enumerate() {
+            for (c, tile) in row.iter().enumerate() {
+                match tile {
+                    Tile2::Empty => new_map[r][c] = '.',
+                    Tile2::Wall => new_map[r][c] = '#',
+                    Tile2::BoxLeft => new_map[r][c] = '[',
+                    Tile2::BoxRight => new_map[r][c] = ']',
+                }
+            }
+        }
+        new_map[self.robot.0][self.robot.1] = '@';
+        for row in new_map.iter() {
+            for ch in row.iter() {
+                print!("{}", ch)
+            }
+            println!();
+        }
+    }
+
+    // Robot, Empty, BoxLeft, BoxRight, Wall
+    // Robot is on empty
+    // exec starts on robot position
+    // scan for "next position": child.
+    // should move the "farthest" box from robot first: postorder traversal
+    // if dir is left or right: no difficulties
+    // if dir is up or right: should scan for both sides
+    // if one of them is blocked by a wall, none should move.
+    // scan first, execute in post-order.
+    fn can_push(&self, from: (usize, usize), to: Dir) -> bool {
+        let (r, c) = from;
+        let ir = r as i32;
+        let ic = c as i32;
+        let (dr, dc) = to.delta();
+        let nr = (ir + dr) as usize;
+        let nc = (ic + dc) as usize;
+        let ahead = self.map[nr][nc];
+        match ahead {
+            Tile2::Empty => true,
+            Tile2::Wall => false,
+            Tile2::BoxLeft => match to {
+                Up | Down => {
+                    let box_left = (nr, nc);
+                    let box_right = (nr, nc + 1);
+                    self.can_push(box_left, to) && self.can_push(box_right, to)
+                }
+                dir => self.can_push((nr, nc), dir),
+            },
+            Tile2::BoxRight => match to {
+                Up | Down => {
+                    let box_right = (nr, nc);
+                    let box_left = (nr, nc - 1);
+                    self.can_push(box_left, to) && self.can_push(box_right, to)
+                }
+                dir => self.can_push((nr, nc), dir),
+            },
+        }
+    }
+    fn push(&mut self, from: (usize, usize), to: Dir) {
+        let (r, c) = from;
+        let ir = r as i32;
+        let ic = c as i32;
+        let (dr, dc) = to.delta();
+        let nr = (ir + dr) as usize;
+        let nc = (ic + dc) as usize;
+        let me = self.map[r][c];
+
+        let ahead = self.map[nr][nc];
+        if ahead == Tile2::Empty {
+            self.map[nr][nc] = me;
+            return;
+        }
+        match to {
+            Left | Right => {
+                self.push((nr, nc), to);
+                self.map[nr][nc] = me;
+            }
+            Up | Down => match ahead {
+                Tile2::BoxLeft => {
+                    self.push((nr, nc), to);
+                    self.map[nr][nc] = me;
+                    self.push((nr, nc + 1), to);
+                    self.map[nr][nc + 1] = Tile2::Empty;
+                }
+                Tile2::BoxRight => {
+                    self.push((nr, nc), to);
+                    self.map[nr][nc] = me;
+                    self.push((nr, nc - 1), to);
+                    self.map[nr][nc - 1] = Tile2::Empty;
+                }
+                _ => unimplemented!(),
+            },
+        }
+    }
+
+    fn exec(&mut self, dir: Dir) {
+        let from = self.robot;
+        if !self.can_push(from, dir) {
+            return;
+        }
+        let (r, c) = from;
+        let ir = r as i32;
+        let ic = c as i32;
+        let (dr, dc) = dir.delta();
+        let nr = (ir + dr) as usize;
+        let nc = (ic + dc) as usize;
+
+        self.push(from, dir);
+        self.robot = (nr, nc);
+    }
+
+    fn score(&self) -> usize {
+        let mut ans = 0;
+        let (mr, mc) = self.size();
+        for r in 0..mr {
+            for c in 0..mc {
+                if self.map[r][c] == Tile2::BoxLeft {
+                    ans += 100 * r + c
+                }
+            }
+        }
+        ans
     }
 }
 
@@ -191,7 +361,22 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^",
     }
 
     fn solve_part_2(_input: String) -> String {
-        String::from("0")
+        let mut parts = _input.split("\n\n");
+        let mut sys = System2::from(parts.next().unwrap());
+
+        let cmds: Vec<Dir> = parts
+            .next()
+            .unwrap()
+            .lines()
+            .map(|line| line.chars().map(Dir::from))
+            .flatten()
+            .collect();
+
+        for cmd in cmds {
+            sys.exec(cmd);
+        }
+
+        sys.score().to_string()
     }
 }
 
@@ -371,9 +556,25 @@ mod day15_tests {
     }
 
     #[test]
+    fn test_part_2_smaller() {
+        let input = "#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^"
+            .to_string();
+        let ans = Day15::solve_part_2(input); // 105+207+306
+        assert_eq!(ans, "618");
+    }
+
+    #[test]
     fn test_part_2() {
         let input = Day15::test_input();
         let ans = Day15::solve_part_2(input);
-        assert_eq!(ans, "");
+        assert_eq!(ans, "9021");
     }
 }
