f9b20e94c6eb0e3577ff99278f004ff8eee236e9
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2024-12-24 16:26:10 +0900

    Part 2

--- src/solutions/day23.rs
+++ src/solutions/day23.rs
@@ -1,15 +1,28 @@
+use std::collections::HashSet;
+
 use super::Solution;
 
 const MAX_NODE: usize = 25 * 26 + 25;
+type CONNECTED = [[bool; MAX_NODE]; MAX_NODE];
 
-const T: usize = (('t' as u32 - 'a' as u32) as usize) * 26;
+fn id_to_node(id: usize) -> String {
+    let a = id / 26;
+    let b = id % 26;
+    let a = a as u8;
+    let a = 'a' as u8 + a;
+    let a = a as char;
+    let b = b as u8;
+    let b = 'a' as u8 + b;
+    let b = b as char;
+    format!("{}{}", a, b)
+}
 
 fn node_id(node: &str) -> usize {
     let mut chars = node.chars();
     let a = chars.next().unwrap();
     let b = chars.next().unwrap();
-    let a = a as u32 - 'a' as u32;
-    let b = b as u32 - 'a' as u32;
+    let a = a as u8 - 'a' as u8;
+    let b = b as u8 - 'a' as u8;
     (a as usize) * 26 + (b as usize)
 }
 
@@ -18,6 +31,72 @@ fn starts_with_t(num: usize) -> bool {
     let t = t as usize;
     let t = t * 26;
     t <= num && num < t + 26
+}
+
+fn parse(input: String) -> (Vec<usize>, CONNECTED) {
+    let mut seen: [bool; MAX_NODE] = [false; MAX_NODE];
+    let mut nodes = vec![];
+    let mut connected = [[false; MAX_NODE]; MAX_NODE];
+    for line in input.lines() {
+        let mut words = line.split("-");
+        let a = node_id(words.next().unwrap());
+        let b = node_id(words.next().unwrap());
+        if !seen[a] {
+            seen[a] = true;
+            nodes.push(a);
+        }
+        if !seen[b] {
+            seen[b] = true;
+            nodes.push(b);
+        }
+
+        connected[a][b] = true;
+        connected[b][a] = true;
+    }
+    nodes.sort();
+    (nodes, connected)
+}
+
+fn build_three(nodes: &[usize], connected: &CONNECTED) -> HashSet<Vec<usize>> {
+    let l = nodes.len();
+    let mut set = HashSet::new();
+    for i in 0..l {
+        for j in i + 1..l {
+            for k in j + 1..l {
+                let i = nodes[i];
+                let j = nodes[j];
+                let k = nodes[k];
+
+                if connected[i][j] && connected[j][k] && connected[k][i] {
+                    set.insert(vec![i, j, k]);
+                }
+            }
+        }
+    }
+    set
+}
+
+fn build_for_more(
+    nodes: &[usize],
+    connected: &CONNECTED,
+    prev: HashSet<Vec<usize>>,
+) -> HashSet<Vec<usize>> {
+    let l = nodes.len();
+    let mut set = HashSet::new();
+    for mut v in prev {
+        for i in 0..l {
+            let i = nodes[i];
+            if v.contains(&i) {
+                continue;
+            }
+            if v.iter().all(|&j| connected[i][j]) {
+                v.push(i);
+                v.sort();
+                set.insert(v.clone());
+            }
+        }
+    }
+    set
 }
 
 pub struct Day23;
@@ -61,44 +140,46 @@ td-yn",
     }
 
     fn solve_part_1(input: String) -> String {
-        let mut is_node: [bool; MAX_NODE] = [false; MAX_NODE];
-        let mut connected: [[bool; MAX_NODE]; MAX_NODE] = [[false; MAX_NODE]; MAX_NODE];
-        for line in input.lines() {
-            let mut nodes = line.split("-");
-            let a = node_id(nodes.next().unwrap());
-            let b = node_id(nodes.next().unwrap());
-            is_node[a] = true;
-            is_node[b] = true;
-            connected[a][b] = true;
-            connected[b][a] = true;
-        }
-        let mut ans = 0;
-        for i in 0..MAX_NODE {
-            for j in i + 1..MAX_NODE {
-                for k in j + 1..MAX_NODE {
-                    if !is_node[i] || !is_node[j] || !is_node[k] {
-                        continue;
-                    }
-                    if !starts_with_t(i) && !starts_with_t(j) && !starts_with_t(k) {
-                        continue;
-                    }
-                    if connected[i][j] && connected[j][k] && connected[k][i] {
-                        ans += 1;
-                    }
-                }
-            }
-        }
+        let (nodes, connected) = parse(input);
+        let threes = build_three(&nodes, &connected);
+        let ans = threes
+            .into_iter()
+            .map(|v| (v[0], v[1], v[2]))
+            .filter(|&(i, j, k)| starts_with_t(i) || starts_with_t(j) || starts_with_t(k))
+            .count();
         ans.to_string()
     }
 
-    fn solve_part_2(_input: String) -> String {
-        String::from("0")
+    fn solve_part_2(input: String) -> String {
+        let (nodes, connected) = parse(input);
+        let mut prev = build_three(&nodes, &connected);
+        let mut num = 3;
+        while prev.len() != 1 {
+            println!("{} {}", num, prev.len());
+            num += 1;
+            prev = build_for_more(&nodes, &connected, prev);
+        }
+
+        println!("{} {}", num, prev.len());
+        prev.into_iter()
+            .next()
+            .unwrap()
+            .into_iter()
+            .map(id_to_node)
+            .collect::<Vec<_>>()
+            .join(",")
     }
 }
 
 #[cfg(test)]
 mod day23_tests {
     use super::*;
+
+    #[test]
+    fn test_id() {
+        let id = "co";
+        assert_eq!(id_to_node(node_id(id)), id);
+    }
 
     #[test]
     fn test_part_1() {
